5586095988cbf8db0c1bdf7faf1c46ed34e1a3a7
Daniel Pradera <Daniel Pradera>
2016-03-07 11:16:51 +0100

    0092

--- Responder.java
+++ Responder.java
@@ -1,5 +1,6 @@
 import java.util.Random;
 import java.util.ArrayList;
+import java.util.HashMap;
 /**
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
@@ -11,6 +12,7 @@ public class Responder
 {
     private ArrayList<String>lista;
     private Random resp;
+    private HashMap <String, String> respuesta;
     /**
      * Construct a Responder - nothing to do
      */
@@ -23,14 +25,29 @@ public class Responder
         lista.add("That don't sounds well, you should stop.");
         lista.add("Please, be carefull, but... continue...");
         lista.add("Are you sure that all it it's real?");
+        
+        respuesta = new HashMap<>();
+        respuesta.put("Hello", "Tell us what happends.");
+        respuesta.put("Thanks", " If you have further questions or need assistance, please, contact us again.");
+        respuesta.put("Solved", "Thank you for contacting to our technical Suppport.");
+        respuesta.put("Bye", "Please do not hesitate to contact us again.");
     }
 
     /**
      * Generate a response.
      * @return   A string that should be displayed as the response
      */
-    public String generateResponse()
+    public String generateResponse(String input)
     {
-        return lista.get(resp.nextInt(lista.size()));
+        String response;
+        if (respuesta.get(input) != null)
+        {
+            response = respuesta.get(input);
+        }
+        else
+        {
+            response = lista.get(resp.nextInt(lista.size()));
+        }
+        return response;
     }
 }
